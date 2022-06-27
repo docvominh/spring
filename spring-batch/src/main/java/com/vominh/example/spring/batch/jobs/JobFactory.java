d42c9c04c0a5f5a43b9e08fe9d64a8202ee2f7c1
Minh <minh.phamduc@axonactive.com>
2022-06-27 11:59:01 +0700

    update spring batch, add schedule

--- spring-batch/src/main/java/com/vominh/example/spring/batch/jobs/JobFactory.java
+++ spring-batch/src/main/java/com/vominh/example/spring/batch/jobs/JobFactory.java
@@ -1,46 +1,49 @@
 package com.vominh.example.spring.batch.jobs;
 
 import com.vominh.example.spring.batch.config.JobCompletionNotificationListener;
+import com.vominh.example.spring.batch.dto.PopulationDto;
 import com.vominh.example.spring.batch.tasks.Processor;
 import com.vominh.example.spring.batch.tasks.Reader;
 import com.vominh.example.spring.batch.tasks.Writer;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
 
-@EnableBatchProcessing
-public class SimpleJob {
+import java.util.Random;
+
+@Service
+public class JobFactory {
+    public final JdbcTemplate jdbcTemplate;
     public final JobBuilderFactory jobBuilderFactory;
     public final StepBuilderFactory stepBuilderFactory;
 
-    public SimpleJob(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+    public JobFactory(JdbcTemplate jdbcTemplate, JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+        this.jdbcTemplate = jdbcTemplate;
         this.jobBuilderFactory = jobBuilderFactory;
         this.stepBuilderFactory = stepBuilderFactory;
 
     }
 
-    @Bean
-    public Job readAndProcessAndWriteJob(JobCompletionNotificationListener listener) {
-
+    public Job createReadAndProcessAndWriteJob() {
 
         var r = new Reader();
         var p = new Processor();
-        var w = new Writer();
+        var w = new Writer(jdbcTemplate);
 
-        var step1 = stepBuilderFactory.get("step1").chunk(10)
+
+        var step1 = stepBuilderFactory.get("step1")
+                .<PopulationDto, PopulationDto>chunk(10)
                 .reader(r)
                 .processor(p)
                 .writer(w)
                 .build();
 
-//        var step2 = stepBuilderFactory.get("step2").chunk(10).build();
-
-        return jobBuilderFactory.get("readAndProcessAndWriteJob")
+        return jobBuilderFactory.get("readAndProcessAndWriteJob" + new Random().nextInt(10000))
                 .incrementer(new RunIdIncrementer())
-                .listener(listener)
+                .listener(new JobCompletionNotificationListener())
                 .flow(step1)
                 .end()
                 .build();
